9c30f623293d0d7e55689f00effde41753f04680
Pierre <72155300+Pierre36@users.noreply.github.com>
2023-06-03 11:28:47 +0200

    Add css class for dropdown menus

--- src/components/SideNavMenu.vue
+++ src/components/SideNavMenu.vue
@@ -62,7 +62,7 @@ export default {
 <template>
   <nav ref="nav">
     <ul>
-      <li v-for="panel in displayedPanels" class="navItem" :class="{ 'active': currentPanel == panel.id && !sidePanelCollapsed }">
+      <li v-for="panel in displayedPanels" class="navItem" :class="{ 'selected': currentPanel == panel.id && !sidePanelCollapsed }">
         <button class="icon-button" @click="changePanel(panel.id)">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" role="img">
             <title>{{ panel.name }}</title>
@@ -78,8 +78,8 @@ export default {
               d="M207.858-432Q188-432 174-446.142q-14-14.141-14-34Q160-500 174.142-514q14.141-14 34-14Q228-528 242-513.858q14 14.141 14 34Q256-460 241.858-446q-14.141 14-34 14Zm272 0Q460-432 446-446.142q-14-14.141-14-34Q432-500 446.142-514q14.141-14 34-14Q500-528 514-513.858q14 14.141 14 34Q528-460 513.858-446q-14.141 14-34 14Zm272 0Q732-432 718-446.142q-14-14.141-14-34Q704-500 718.142-514q14.141-14 34-14Q772-528 786-513.858q14 14.141 14 34Q800-460 785.858-446q-14.141 14-34 14Z" />
           </svg>
         </button>
-        <ul id="moreMenu" ref="moreMenu" :class="{ 'collapsed': moreMenuCollapsed }">
-          <li v-for="panel in hiddenPanels" :class="{ 'active': currentPanel == panel.id && !sidePanelCollapsed }" @click="changePanel(panel.id)">
+        <ul id="moreMenu" ref="moreMenu" class="dropdownMenu" v-if="!moreMenuCollapsed">
+          <li v-for="panel in hiddenPanels" :class="{ 'selected': currentPanel == panel.id && !sidePanelCollapsed }" @click="changePanel(panel.id)">
             <span class="dot"></span>
             <span>{{ panel.name }}</span>
           </li>
@@ -94,28 +94,18 @@ nav {
   height: calc(100% - var(--collapseButton-height));
 }
 
-ul {
-  margin: 0;
-  padding: 0;
-  list-style: none;
-  text-align: center;
-}
-
 .navItem {
   padding: 0.35rem 0rem;
   border-left: 0.12rem solid transparent;
   margin-bottom: 0.5rem;
+  text-align: center;
 }
 
-.navItem.active {
+.navItem.selected {
   border-left-color: var(--gray-100);
 }
 
-.navItem.active {
-  border-left-color: var(--gray-100);
-}
-
-.navItem.active .icon-button {
+.navItem.selected .icon-button {
   color: var(--gray-100);
 }
 
@@ -128,41 +118,13 @@ ul {
 }
 
 #moreMenu {
-  position: absolute;
   top: 0;
   left: 100%;
-  z-index: 100;
   margin-left: 0.25rem;
   margin-top: 0.25rem;
-  padding: 0.25rem;
-  color: var(--gray-300);
-  background-color: var(--gray-500);
-  border: 0.15rem solid var(--gray-400);
-  border-radius: 0.25rem;
-  white-space: nowrap;
-  text-align: left;
 }
 
-#moreMenu.collapsed {
-  display: none;
-}
-
-#moreMenu li {
-  display: flex;
-  cursor: pointer;
-  border-radius: 0.25rem;
-  padding: 0.25rem 0.5rem;
-}
-
-#moreMenu li:hover {
-  background-color: var(--blue-200);
-}
-
-#moreMenu li.active {
-  color: var(--gray-100);
-}
-
-#moreMenu li.active .dot {
+#moreMenu li.selected .dot {
   background-color: var(--gray-100);
 }
 
@@ -171,6 +133,6 @@ ul {
   width: 0.5rem;
   background-color: var(--gray-300);
   border-radius: 50%;
-  margin: auto 0.75rem auto 0rem;
+  margin: auto 0.75rem auto 0.25rem;
 }
 </style>
