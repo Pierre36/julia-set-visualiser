60311f0b18f9a0360537cdaae3fea2dc27578501
Pierre <72155300+Pierre36@users.noreply.github.com>
2023-06-18 16:39:19 +0200

    Create TopNav base layout

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,16 +1,45 @@
 <script>
+import FormSelect from "./FormSelect.vue";
+
 export default {
   name: "TopNav",
+  components: { FormSelect },
+  data() {
+    return {
+      configurationOptions: [{ id: 0, text: "Default" }],
+      selectedConfiguration: 0,
+    };
+  },
 };
 </script>
 
 <template>
-  <nav id="topNav"></nav>
+  <div id="topNav">
+    <img class="logo" src="../../public/logo.svg" />
+    <h1>Julia Set Visualizer</h1>
+    <FormSelect
+      :options="configurationOptions"
+      :selected="selectedConfiguration"
+    />
+  </div>
 </template>
 
 <style scoped>
 #topNav {
   height: var(--topNav-height);
   background-color: var(--gray-400);
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+  padding-right: 0.5rem;
+}
+
+h1 {
+  flex-grow: 1;
+}
+
+.logo {
+  max-height: var(--topNav-height);
+  padding: 0.5rem;
 }
 </style>
