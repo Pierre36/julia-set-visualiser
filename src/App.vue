840eece8ef1431a0a4cd429dcbf7a9ffdc76e893
Pierre <72155300+Pierre36@users.noreply.github.com>
2023-06-24 09:54:50 +0200

    Add the save method to the toolbar

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,53 @@ import TopNav from "./components/TopNav.vue";
 import SideBar from "./components/SideBar.vue";
 import AnimationFrame from "./components/AnimationFrame.vue";
 import { Configuration } from "./models/Configuration";
+import { Polynomial } from "./models/Polynomial";
+import { Attractor } from "./models/Attractor";
 
 export default {
   name: "App",
   components: { TopNav, SideBar, AnimationFrame },
   data() {
     return {
-      configurations: { 0: Configuration.defaultConfiguration() },
-      selectedConfigurationId: "0",
+      configurations: {
+        DEFAULT: Configuration.defaultConfiguration(),
+        CUSTOM: new Configuration(
+          "CUSTOM",
+          "Custom",
+          1,
+          1,
+          new Polynomial(),
+          "DEFAULT",
+          [0, 0, 0],
+          new Attractor(null, 0, 0, 0, 0, 0),
+          new Attractor(null, 0, 0, 0, 0, 0),
+          []
+        ),
+      },
+      selectedConfigurationId: "DEFAULT",
       configuration: Configuration.defaultConfiguration(),
     };
   },
   created() {
-    this.getConfigurations();
+    this.getJSONConfigurations();
+    this.getStorageConfiguration();
   },
   methods: {
     updateConfiguration(newSelectedId) {
       this.selectedConfigurationId = newSelectedId;
       this.configuration.fillWith(this.configurations[newSelectedId]);
     },
-    async getConfigurations() {
+    getStorageConfiguration() {
+      const localConfiguration = localStorage.getItem("customConfiguration");
+      if (localConfiguration != null) {
+        this.configurations["CUSTOM"] = Configuration.fromJSON(
+          JSON.parse(localConfiguration)
+        );
+        this.selectedConfigurationId = "CUSTOM";
+        this.updateConfiguration(this.selectedConfigurationId);
+      }
+    },
+    async getJSONConfigurations() {
       const { default: json } = await import("./assets/configurations.json");
       json.forEach((jsonConfiguration) => {
         this.configurations[jsonConfiguration.id] =
@@ -37,6 +64,7 @@ export default {
   <TopNav
     :configurations="configurations"
     :selectedConfigurationId="selectedConfigurationId"
+    :configuration="configuration"
     @update:selectedConfigurationId="
       (newSelectedId) => updateConfiguration(newSelectedId)
     "
--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,12 +1,15 @@
 <script>
+import { Configuration } from "../models/Configuration";
 import FormSelect from "./FormSelect.vue";
+import Toast from "./Toast.vue";
 
 export default {
   name: "TopNav",
-  components: { FormSelect },
+  components: { FormSelect, Toast },
   props: {
     configurations: { type: Object, required: true },
     selectedConfigurationId: { type: String, required: true },
+    configuration: { type: Configuration, required: true },
   },
   emits: ["update:selectedConfigurationId"],
   computed: {
@@ -19,6 +22,15 @@ export default {
         });
       });
       return configurationOptions;
+    },
+  },
+  methods: {
+    saveConfiguration() {
+      localStorage.setItem(
+        "customConfiguration",
+        JSON.stringify(this.configuration.toJSON())
+      );
+      this.$refs.toast.show();
     },
   },
 };
@@ -35,6 +47,25 @@ export default {
         (newSelected) => $emit('update:selectedConfigurationId', newSelected)
       "
     />
+    <button
+      class="icon-button"
+      @click="saveConfiguration"
+      :disabled="selectedConfigurationId != 'CUSTOM'"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 -960 960 960"
+        role="img"
+      >
+        <title>Save</title>
+        <path
+          fill="currentColor"
+          fill-rule="evenodd"
+          d="M840-683v503q0 24-18 42t-42 18H180q-24 0-42-18t-18-42v-600q0-24 18-42t42-18h503l157 157Zm-60 27L656-780H180v600h600v-476ZM479.765-245Q523-245 553.5-275.265q30.5-30.264 30.5-73.5Q584-392 553.735-422.5q-30.264-30.5-73.5-30.5Q437-453 406.5-422.735q-30.5 30.264-30.5 73.5Q376-306 406.265-275.5q30.264 30.5 73.5 30.5ZM233-584h358v-143H233v143Zm-53-72v476-600 124Z"
+        />
+      </svg>
+    </button>
+    <Toast ref="toast" text="Custom configuration saved" />
   </div>
 </template>
 
@@ -43,7 +74,9 @@ export default {
   height: var(--topNav-height);
   background-color: var(--gray-400);
   display: grid;
-  grid-template-columns: var(--sideNav-width) auto 15rem;
+  grid-template-columns:
+    var(--sideNav-width) auto minmax(min-content, 15rem)
+    max-content;
   align-items: center;
   gap: 0.75rem;
   padding-right: 0.5rem;
