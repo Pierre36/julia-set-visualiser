e2dd1f187b52cafe2921dfebab4782ee6c3622e2
Pierre <72155300+Pierre36@users.noreply.github.com>
2023-07-23 18:50:01 +0200

    Fix ComplexMultiplication test

--- src/models/__tests__/ComplexMultiplication.spec.js
+++ src/models/__tests__/ComplexMultiplication.spec.js
@@ -27,11 +27,11 @@ it("properly returns a complex number when given two complex numbers", () => {
 
 describe("getAtTime", () => {
   it("properly returns the product at time", () => {
-    const coefficient1 = new Complex(1, 0);
+    const coefficient1 = new ComplexCircle(new Complex(1, 0), 1, 2000);
     const coefficient2 = new Complex(2, 0);
 
     const product = new ComplexMultiplication(coefficient1, coefficient2);
 
-    expect(product.getAtTime(0)).toEqual(new Complex(2, 0));
+    expect(product.getAtTime(0)).toEqual(new Complex(4, 0));
   });
 });
