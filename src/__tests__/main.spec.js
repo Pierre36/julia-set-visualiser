92d39f7db4295e4bfe8b49da06decb68d82b4cc4
Pierre <72155300+Pierre36@users.noreply.github.com>
2023-07-24 21:56:19 +0200

    Remove test for main.js

--- src/__tests__/main.spec.js
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect, vi } from "vitest";
-
-describe("App creation", () => {
-  it("does not raise any error", async () => {
-    console.error = vi.fn();
-    await import("../main");
-    expect(console.error).not.toBeCalled();
-  });
-});
