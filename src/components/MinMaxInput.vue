34f753e1c157a77b48ac5af2ddd0e6c20bc76192
Pierre <72155300+Pierre36@users.noreply.github.com>
2023-08-04 18:39:28 +0200

    Implement and test MinMaxInput component

---
+++ src/components/MinMaxInput.vue
@@ -0,0 +1,59 @@
+<script>
+import NumberInput from "./NumberInput.vue";
+
+export default {
+  name: "MinMaxInput",
+  components: { NumberInput },
+  emits: ["update:minValue", "update:maxValue"],
+  props: {
+    minValue: { type: Number, default: 0 },
+    maxValue: { type: Number, default: 0 },
+    min: { type: Number, default: undefined },
+    max: { type: Number, default: undefined },
+    step: { type: Number, default: 1 },
+    integerOnly: { type: Boolean, default: false },
+    minLabel: { type: String, default: "" },
+    maxLabel: { type: String, default: "" },
+    level: { type: Number, default: 5 },
+  },
+  computed: {
+    heading() {
+      return `h${this.level}`;
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="min-max-input">
+    <component :is="heading">Min:</component>
+    <NumberInput
+      :value="minValue"
+      :min="min"
+      :max="maxValue"
+      :step="step"
+      :integerOnly="integerOnly"
+      :label="minLabel"
+      @update:value="(newMinValue) => $emit('update:minValue', newMinValue)"
+    />
+    <component :is="heading">Max:</component>
+    <NumberInput
+      :value="maxValue"
+      :min="minValue"
+      :max="max"
+      :step="step"
+      :integerOnly="integerOnly"
+      :label="maxLabel"
+      @update:value="(newMaxValue) => $emit('update:maxValue', newMaxValue)"
+    />
+  </div>
+</template>
+
+<style scoped>
+.min-max-input {
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  align-items: center;
+  gap: 0.5rem;
+}
+</style>
